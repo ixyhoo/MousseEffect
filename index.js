9fad65d2b96cfbfef222ff286273bf23b079423f
ixyhoo <ixyhoo@outlook.fr>
2023-05-05 10:45:23 +0200

    project finished

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
 
+const mouses = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+    mouses.forEach(mouse => {
+        mouse.style.top = e.y + "px";
+        mouse.style.left = e.x + "px";
+    })
+})
+
+/*window.addEventListener("mousemove", (e) => {
+
+    cursor.style.top = e.y + "px";
+    cursor.style.left = e.x + "px";
+
+    mouse1.style.top = e.y + "px";
+    mouse1.style.left = e.x + "px";
+
+    mouse2.style.top = e.y + "px";
+    mouse2.style.left = e.x + "px";
+
+
+})*/
+
+
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
 
 // S'assurer que les liens sont clickables
